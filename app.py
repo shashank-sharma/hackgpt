5ca6b384f8ea2ec05799ab47c0041b35707c2a02
Shashank Sharma <shashank.sharma98@gmail.com>
2023-05-04 11:05:21 +0530

    More scam

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # Import necessary libraries
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 import util
+import PyPDF2
+import io
 
 # Initialize the Flask application
 app = Flask(__name__)
@@ -14,7 +16,19 @@ def process():
 
 @app.route('/resume', methods=['POST'])
 def process_resume():
-    resume = request.json['resume']
+    pdf_file = "/users/shashank/Downloads/" + request.json['resume']
+    with open(pdf_file, 'rb') as file:
+        pdf_reader = PyPDF2.PdfReader(io.BytesIO(file.read()))
+
+    # Read the content of the PDF file and store it in a single string
+    pdf_content = ""
+    for page_num in range(len(pdf_reader.pages)):
+        page = pdf_reader.pages[page_num]
+        pdf_content += page.extract_text()
+
+    resume = pdf_content
+    print(resume)
+
     job_desc = request.json['job_desc']
     clean_resume = util.clean_string(resume)
     clean_job_desc = util.clean_string(job_desc)
@@ -27,6 +41,7 @@ def process_resume():
             score += 1
     req_skills_len = len(job_skills)
     match = round(score / req_skills_len * 100, 1)
+    print("done", match)
 
     return {
         "resume_skills": resume_skills,
